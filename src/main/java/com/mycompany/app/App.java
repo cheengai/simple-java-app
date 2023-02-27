6ecd85e69c3c6afc0c46b38c2ac2be4776cb1fb5
Chin Chee Ngai <chincheengai@gmail.com>
2023-02-27 07:56:20 +0000

    new change !

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Oh my god!");
+        System.out.println("this become interesting again!");
     }
 
     public String getMessage() {
